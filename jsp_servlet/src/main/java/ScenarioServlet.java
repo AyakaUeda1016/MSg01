fe5ea45f80af853221d8d760162d7255afc14016
Ayaka Ueda <ayasura36@gmail.com>
2025-11-18 16:03:29 +0900

    追加(上田):ScenarioServlet追加

---
+++ jsp_servlet/src/main/java/ScenarioServlet.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SenalioServlet
+ */
+@WebServlet("/ScenalioServlet")
+public class ScenarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ScenarioServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		String strNo = request.getParameter("scenarioId");
+		String sb = request.getParameter("sb");
+		
+		System.out.println(strNo);
+		System.out.println(sb);
+		
+		if(sb.equals("decide")) {
+			RequestDispatcher rd = request.getRequestDispatcher("./simulation.jsp");
+			rd.forward(request, response);
+		}else if(sb.equals("home")) {
+			RequestDispatcher rd = request.getRequestDispatcher("./home.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
