91bc17f7db8ad6b8cc5d308ca7f1f5a8549afdc2
Ayaka Ueda <ayasura36@gmail.com>
2025-11-18 14:55:39 +0900

    追加(上田):HomeServletを追加。home.jspもそれに伴い変更。

---
+++ jsp_servlet/src/main/java/HomeServlet.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/HomeServlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		String sb = request.getParameter("sb");
+		System.out.println(sb);
+		
+		if(sb.equals("senario")) {
+			RequestDispatcher rd = request.getRequestDispatcher("./scenario.jsp");
+			rd.forward(request, response);
+		}else if(sb.equals("growth_record")) {
+			RequestDispatcher rd = request.getRequestDispatcher("./growth_record.jsp");
+			rd.forward(request, response);
+		}else if(sb.equals("setting")) {
+			RequestDispatcher rd = request.getRequestDispatcher("./settings.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
